837cc3a2a5a420c8398f51ce4a2e00f8caf7850e
danielAdama <adamadaniel321.com>
2022-09-08 02:20:58 +0100

    Initial Commit

---
+++ tests/test_car.py
@@ -0,0 +1,4 @@
+from config import config
+from imutils import paths
+from detector.car import CarDetector
+
---
+++ tests/test_googleImageScraper.py
@@ -0,0 +1,10 @@
+import pytest
+# import sys
+# sys.path.append('/home/daniel/Desktop/programming/pythondatascience/datascience/computerVision/car-brand-classifier')
+from scrape import googleImageScraper
+from unittest.mock import Mock
+
+# def test_imageScraper():
+#     actual = googleImageScraper.main()
+#     expected = None
+#     assert actual is expected
