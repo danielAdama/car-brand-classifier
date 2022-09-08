9869076fa49533b8ceb3256a161a0811e9b4e9b1
danielAdama <adamadaniel321.com>
2022-09-08 10:47:37 +0100

    Added the DNN weight and cfg path

--- config/config.py
+++ config/config.py
@@ -1,7 +1,9 @@
 import os 
 import cv2
 
-BASEPATH = "/home/daniel/Desktop/programming/pythondatascience/datascience/computerVision/dataset/car_brand/custome_data"
+WEIGHTS = 'dnn_model/yolov4.weights'
+CFG = 'dnn_model/yolov4.cfg'
+DATAPATH = "/home/daniel/Desktop/programming/pythondatascience/datascience/computerVision/dataset/car_brand/custome_data"
 # BASEPATH = "/home/daniel/Desktop/programming/pythondatascience/datascience/computerVision/dataset/car_brand/custome_new"
 BRAND_NAMES = ["Audi", "BMW", "Toyota", "Mercedes", "Lexus", "Honda"]
 NUM_IMAGES_TO_SCRPE = 80
