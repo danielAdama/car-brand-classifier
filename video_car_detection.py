ce3e6ec94d9e4cfd4dd47d4744438b515797d7ce
danielAdama <adamadaniel321.com>
2022-09-10 13:43:23 +0100

    Loaded the DNN model

--- video_car_detection.py
+++ video_car_detection.py
@@ -1,3 +1,5 @@
+from pyexpat import model
+from turtle import width
 import cv2
 import imutils
 import time
@@ -5,4 +7,24 @@ import numpy as np
 from config import config
 from detector.car import CarDetector
 
+# Load the model
+car = CarDetector()
 
+webcam = cv2.VideoCapture(config.VIDEO)
+time.sleep(2.0)
+if (webcam.isOpened == False):
+    print("\nUnable to read video")
+
+
+while True:
+    success, frame = webcam.read()
+    if success == True:
+        frame = imutils.resize(frame, 700)
+        (height, width) = frame.shape[:2]
+
+        cv2.imshow("Live", frame)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+    else:
+        break
