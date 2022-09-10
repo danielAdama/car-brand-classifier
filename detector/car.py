fcd3da5ed94a6fdde47a24bcb5405793479c3605
danielAdama <adamadaniel321.com>
2022-09-10 13:56:02 +0100

    Changed vehicle_detected input from image to frame

--- detector/car.py
+++ detector/car.py
@@ -1,4 +1,3 @@
-from distutils.command.config import config
 import cv2
 from config import config
 
@@ -9,7 +8,7 @@ class CarDetector():
         
     """
     def __init__(self):
-        
+
         net = cv2.dnn.readNet(config.WEIGHTS, config.CFG)
         self.model = cv2.dnn_DetectionModel(net)
         self.model.setInputParams(size=(732, 732), scale=1/255)
@@ -17,14 +16,15 @@ class CarDetector():
         # Allow class containing Cars only
         self.classe_allowed = 2
     
-    def vehicle_detected(self, image):
+    def vehicle_detected(self, frame):
         car_boxes = []
-        class_ids, confidences, boxes = self.model.detect(image, nmsThreshold=0.4)
+        class_ids, confidences, boxes = self.model.detect(frame, nmsThreshold=0.4)
         for class_id, confidence, box in zip(class_ids, confidences, boxes):
+            print(class_id, confidence, box)
             # Skip detections with low confidence
-            if confidence < 0.82:
+            if (confidence < 0.82):
                 continue
-            if class_id == self.classe_allowed:
+            if (class_id == self.classe_allowed):
                 car_boxes.append(box)
         
         return car_boxes
