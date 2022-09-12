c761109a5417082fb10c999595e82487ed3a2c37
danielAdama <adamadaniel321.com>
2022-09-12 01:32:47 +0100

    Changed to yolov4-tiny

--- tests/test_car.py
+++ tests/test_car.py
@@ -3,18 +3,20 @@ import imutils
 import numpy as np
 import cv2
 import os
+import sys
+sys.path.append('/home/daniel/Desktop/programming/pythondatascience/datascience/computerVision/car-brand-classifier')
 from detector.car import CarDetector
 
 def test_car():
-    cd = CarDetector(weight='yolov4.weights', cfg='yolov4.cfg')
-    image = os.path.join(os.getcwd(),"tests/pexels-svh-2684219.jpg")
+    cd = CarDetector(weight='yolov4-tiny.weights', cfg='yolov4-tiny.cfg')
+    image = os.path.join(os.getcwd(),"tests/pexels-nordic-overdrive-627719.jpg")
     frame = cv2.imread(image)
     frame = imutils.resize(frame, 900)
     (height, width) = frame.shape[:2]
     frame = cv2.resize(frame, (width, height))
     boxes = cd.vehicle_detected(frame)
+    # Actual representation
     actual = np.array(boxes[0], dtype='int32')
-    expected = np.array([115, 213, 718, 223], dtype='int32')
-    # expected = [115, 213, 718, 223]
+    expected = np.array([168, 176, 486, 172], dtype='int32')
     assert len(actual) == 4
     assert np.array_equal(actual, expected) is True
