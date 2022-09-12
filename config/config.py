9d757284f81e4b0a14906b1312c842288f04cc98
danielAdama <adamadaniel321.com>
2022-09-12 01:22:45 +0100

    Added Traffic.mp4

--- config/config.py
+++ config/config.py
@@ -1,9 +1,8 @@
 import os 
 import cv2
 
-WEIGHTS = 'dnn_model/yolov4.weights'
 EXAMPLE_IMGS = 'examples'
-CFG = 'dnn_model/yolov4.cfg'
+VIDEO = 'Traffic.mp4'
 DATAPATH = "/home/daniel/Desktop/programming/pythondatascience/datascience/computerVision/dataset/car_brand/custome_data"
 # BASEPATH = "/home/daniel/Desktop/programming/pythondatascience/datascience/computerVision/dataset/car_brand/custome_new"
 BRAND_NAMES = ["Audi", "BMW", "Toyota", "Mercedes", "Lexus", "Honda"]
