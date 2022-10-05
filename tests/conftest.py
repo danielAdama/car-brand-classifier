b6c0cff1fd7092bdd4a364b4c5ceb2d375088410
danielAdama <adamadaniel321@gmail.com>
2022-10-04 23:29:54 -0700

    Changed to image to test1.jpg

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,5 @@ import os
 @pytest.fixture
 def post_response_of_v1_image_endpoint_for_test_jpg():
     url = 'http://localhost:8080/v1/image'
-    response = requests.post(url, files={'image': open(os.path.join(os.getcwd(),"tests/test.jpg"), 'rb')})
+    response = requests.post(url, files={'image': open(os.path.join(os.getcwd(),"tests/test1.jpg"), 'rb')})
     return response
