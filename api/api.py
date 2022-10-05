f520ed58b34cd938049340cc58879c705701854e
danielAdama <adamadaniel321@gmail.com>
2022-10-04 23:27:47 -0700

    Added argparse for command line

--- api/api.py
+++ api/api.py
@@ -1,20 +1,15 @@
-import time
+import argparse
 from flask import Flask, request, jsonify
 import os
-import cv2
-import io
-import json
 import pandas as pd
 import sys
-sys.path.append(r'C:\Users\DELL\Desktop\programming\computerVision\car-brand-classifier')
+sys.path.append(os.getcwd())
 from config import config
 from yolo_detect_images import detectObject
-from PIL import Image
-import numpy as np
-import imutils
+
 
 app = Flask(__name__)
-os.environ['FLASK_DEBUG']="development"
+# os.environ['FLASK_DEBUG']="development"
 
 
 @app.route("/v1/image", methods=["POST"])
@@ -26,7 +21,10 @@ def detect():
     if request.files.get("image"):
         image_file = request.files["image"]
         result = detectObject(image_file)
-    return pd.Series(result).to_json(), 200
+    return pd.Series(result).to_json()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    parser = argparse.ArgumentParser(description="Car Classifier Api exposing YOLOv4")
+    parser.add_argument("-p", "--port", default=8080, type=int, help="port number")
+    args = parser.parse_args()
+    app.run(host='0.0.0.0', debug=True, port=args.port)
