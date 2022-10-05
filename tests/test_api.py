445adb1b64eee81d061c3bb76e0ae603b2b1dcba
danielAdama <adamadaniel321@gmail.com>
2022-10-04 22:34:22 -0700

    Test for api image endpoint

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,13 +1,10 @@
 import pytest
-from flask import Flask, request, jsonify
-from api.api import detect
+import requests
+import os
 
-def test_post_route_success():
-    app = Flask(__name__)
-    client = app.test_client()
-    # Testing the '/v1/image' route
+
+def test_api_post_image_endpoint():
     url = 'http://localhost:8080/v1/image'
-
-    response = client.post(url, files={'image': open('test.jpg', 'rb')})
-
-
+    response = requests.post(url, files={'image': open(os.path.join(os.getcwd(),"tests/test.jpg"), 'rb')})
+    # response.
+    assert response.status_code == 200
