15ceb7d078b57055f97ede4c6ab511c4f286ba65
danielAdama <adamadaniel321@gmail.com>
2022-10-04 23:23:58 -0700

    Created a function to return the response of the post request

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,5 +7,3 @@ def post_response_of_v1_image_endpoint_for_test_jpg():
     url = 'http://localhost:8080/v1/image'
     response = requests.post(url, files={'image': open(os.path.join(os.getcwd(),"tests/test.jpg"), 'rb')})
     return response
-
-
