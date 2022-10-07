61d3eab40db64882b9c9ca2e0a5ac891e64687df
danielAdama <adamadaniel321@gmail.com>
2022-10-06 20:49:33 -0700

    Added Token

--- config/config.py
+++ config/config.py
@@ -1,11 +1,19 @@
 import os 
 import cv2
 
+
+
+HTTP_200_OK = 200
+HTTP_400_BAD_REQUEST = 400
+HTTP_403_UNAUTHORIZED = 403
+HTTP_404_NOT_FOUND = 404
+HTTP_500_INTERNAL_SERVER_ERROR = 500
+
 KEYS = {
     "USER_NAME":"admin",
-    "SECRET_KEY":"summer_123."
+    "SECRET_KEY":"summer_123.",
+    "TOKEN":"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiYWRtaW4ifQ.2cKh3mFLAcdiykvlNaNGNatAF3TXmAcBHSYaiEeb1dLgwxb45DaRZqFlB8gdhsQ8i93oiDOgSNMJxuDE6dDigA"
 }
-
 EXAMPLE_IMGS = r'C:/Users/Dell/Desktop/programming/computerVision/car-brand-classifier/examples'
 VIDEO = 'Traffic.mp4'
 BASEPATH = r'C:\Users\DELL\Desktop\programming\computerVision\car-brand-classifier'
