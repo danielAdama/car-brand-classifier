896d4b74fd5d2632eb2b5890fec5acdc72887118
danielAdama <adamadaniel321@gmail.com>
2022-10-06 20:50:44 -0700

    Added header

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,14 @@
 import pytest
 import requests
 import os
+from config import config
 
 @pytest.fixture
 def post_response_of_v1_image_endpoint_for_test_jpg():
     url = 'http://localhost:8080/v1/image'
-    response = requests.post(url, files={'image': open(os.path.join(os.getcwd(),"tests/test1.jpg"), 'rb')})
+    response = requests.post(
+        url, 
+        files={'image': open(os.path.join(os.getcwd(),"tests/test1.jpg"), 'rb')},
+        headers={'access-token':config.KEYS["TOKEN"]}
+    )
     return response
